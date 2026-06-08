a7e2d6eee250489ae22081d0466bc84d41ee67cb
西花 <konglingxing.klx@antgroup.com>
2026-06-08 21:31:06 +0800

    docs/ 不提交，CI 全权构建 + 缓存

--- docs/webapp/chinamap/main.js
+++
@@ -1,1 +0,0 @@
-requirejs.config({paths:{json:"bower_components/requirejs-json/json",text:"bower_components/requirejs-text/text"}}),define("jquery",[],function(){return window.jQuery}),requirejs(["jquery","src/app"],function(e,r){r.draw()});
